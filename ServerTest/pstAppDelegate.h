b14e04d4a8cdef44f3a1edb61cd1bb8d909c4ae1
dvid <contacto@dvidsilva.com>
2014-05-03 19:43:29 -0700

    Initial Commit

---
+++ ServerTest/pstAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  pstAppDelegate.h
+//  ServerTest
+//
+//  Created by Dvid Silva on 5/3/14.
+//  Copyright (c) 2014 postatlantic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface pstAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
